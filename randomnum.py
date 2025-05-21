3c4e4dda9b07aae4c12502c8c657711399e8064c
Асташкина Александра <aleksasha.ast@gmail.com>
2025-05-21 17:59:16 +0300

    Changed variables names

--- randomnum.py
+++ randomnum.py
@@ -1,10 +1,9 @@
-def random_num ():
+def get_answer ():
     import random
     numbers = ["1","2","3","4","5","6","7","8","9","0"]
-    num = ""
-    while len(num) < 4:
+    answer = ""
+    while len(answer) < 4:
         a = random.choice(numbers)
-        if a not in num:
-            num += a
-    return num
-random_num()
+        if a not in answer:
+            answer += a
+    return answer
